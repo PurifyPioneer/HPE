c2d1aa57c66ce33caf0d902baf2ee0e192804c57
Anton Bracke <anton.bracke.com@gmail.com>
2018-03-05 09:44:11 +0100

    ADD: prettyWithVars

--- Pretty.hs
+++ Pretty.hs
@@ -1,33 +1,41 @@
 module Pretty where
 
 import Data.Char (chr)
-import Data.List (intercalate)
+import Data.List (intercalate, find)
 import Type
 import Subst
 
 class Pretty a where
   pretty :: a -> String
+  prettyWithVars :: [(VarIndex, String)] -> a -> String
 
 instance Pretty Term where
-  pretty (Var i) =
-    if i <= 25
-    -- A to Z
-    then chr (65 + i) : ""
-    -- after Z comes A1, B1, ..., Z1, A2
-    else chr (65 + (i `mod` 26)) : show (i `div` 26)
+  pretty term = prettyWithVars [] term
 
-  pretty (Comb "." [x, (Comb "[]" [])]) = "[" ++ pretty x ++ "]"
-  pretty (Comb "." [x, term@(Comb "." y)]) =
-    "[" ++ pretty x ++ ", "
+  prettyWithVars vars (Var i) =
+    case find (\(index, str) -> index == i) vars of
+      Just (_, str) -> str
+      Nothing ->
+        if i <= 25
+          -- A to Z
+          then chr (65 + i) : ""
+          -- after Z comes A1, B1, ..., Z1, A2
+          else chr (65 + (i `mod` 26)) : show (i `div` 26)
+
+  prettyWithVars vars (Comb "." [x, (Comb "[]" [])]) = "[" ++ pretty x ++ "]"
+  prettyWithVars vars (Comb "." [x, term@(Comb "." y)]) =
+    "[" ++ prettyWithVars vars x ++ ", "
     -- deletes first and last list element, in our case parentheses
-    ++ tail (init (pretty term)) ++ "]"
-  pretty (Comb "." [x, y]) = "[" ++ pretty x ++ "|" ++ pretty y ++ "]"
+    ++ tail (init (prettyWithVars vars term)) ++ "]"
+  prettyWithVars vars (Comb "." [x, y]) = "[" ++ prettyWithVars vars x ++ "|" ++ prettyWithVars vars y ++ "]"
 
-  pretty (Comb str []) = str
-  pretty (Comb str xs) = str ++ "(" ++ intercalate ", " (map pretty xs) ++ ")"
+  prettyWithVars vars (Comb str []) = str
+  prettyWithVars vars (Comb str xs) = str ++ "(" ++ intercalate ", " (map (prettyWithVars vars) xs) ++ ")"
 
 instance Pretty Subst where
-  pretty (Subst []) = ""
-  pretty (Subst xs) = "{" ++ intercalate ", " (map prettySubst xs) ++ "}"
+  pretty subst = prettyWithVars [] subst
+
+  prettyWithVars _ (Subst []) = ""
+  prettyWithVars vars (Subst xs) = "{" ++ intercalate ", " (map (prettySubst vars) xs) ++ "}"
     where
-      prettySubst (index, term) = pretty (Var index) ++ " -> " ++ pretty term
+      prettySubst vars (index, term) = prettyWithVars vars (Var index) ++ " -> " ++ prettyWithVars vars term
